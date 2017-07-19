86b109f0ad6db62900ea46655526a56c8300d89b
Zhongyang.Wu <zhongyang.wu@outlook.com>
2017-07-19 22:14:12 +0800

    Vote-19 投票错误提示

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,6 @@ const CanadianService = require("../service/canadian.service");
 let canadian_service = new CanadianService();
 const TokenService = require("../service/token.service");
 let token_service = new TokenService();
-const authorization_filter = require("../filter/authorization.filter");
-const vote_limit = require("../config").vote.vote_limit;
 const path = require('path');
 
 /* GET home page. */
@@ -25,38 +23,50 @@ router.route("/vote")
     .post((req, res, next) => {
         let targets = req.body["target"];
         let token = req.get("X-VOTE-TOKEN");
-        if (!targets || targets.length === 0 || !token || token.length === 0) {
-            // Param Error
-            res.status(404);
-            res.json(new HttpError(404, "No efficient param provided"));
-        }
-        else if (!(targets instanceof Array)) {
-            res.status(401);
-            res.json(new HttpError(401, "Target must be an array"));
-        }
-        else if (targets.length > vote_limit) {
-            res.status(401);
-            res.json(new HttpError(401, "Providing too many target"));
-        }
-        else {
-            vote_service.hasVoted(token, (err, data) => {
-                if (data) {
-                    res.status(403);
-                    res.json(new HttpError(403, "Has voted"));
-                }
-                else {
-                    token_service.save(token, () => {
-                        for (let i = 0; i < targets.length; i++) {
-                            vote_service.vote(new Vote(targets[i], token), () => {
-                                if (i === targets.length - 1) {
-                                    res.json("success");
-                                }
-                            });
-                        }
-                    })
-                }
-            });
-        }
+        let version = req.body["version"];
+        canadian_service.readFile("canadian.list.json",(data)=>{
+            data = JSON.parse(data);
+            let canadians = data.canadians;
+            let current_version = data.version;
+            let limit = data.vote_limit;
+
+            if (!targets || targets.length === 0 || !token || token.length === 0) {
+                // Param Error
+                res.status(404);
+                res.json(new HttpError(404, "No efficient param provided"));
+            }
+            else if (!(targets instanceof Array)) {
+                res.status(401);
+                res.json(new HttpError(401, "Target must be an array"));
+            }
+            else if (targets.length > limit) {
+                res.status(401);
+                res.json(new HttpError(401, "Providing too many target"));
+            }
+            else if(current_version != version){
+                res.status(403);
+                res.json(new HttpError(403, "Page has expired"));
+            }
+            else {
+                vote_service.hasVoted(token, (err, data) => {
+                    if (data) {
+                        res.status(403);
+                        res.json(new HttpError(403, "Has voted"));
+                    }
+                    else {
+                        token_service.save(token, () => {
+                            for (let i = 0; i < targets.length; i++) {
+                                vote_service.vote(new Vote(targets[i], token), () => {
+                                    if (i === targets.length - 1) {
+                                        res.json("success");
+                                    }
+                                });
+                            }
+                        })
+                    }
+                });
+            }
+        })
     })
     .get((req, res, next) => {
         //get the token use to prevent multi vote
@@ -65,8 +75,9 @@ router.route("/vote")
             canadian_service.readFile("canadian.list.json", (data) => {
                 let canadians = JSON.parse(data);
                 let result = {};
-                result.limit = vote_limit;
-                result.canadians = canadians;
+                result.limit = canadians.vote_limit;
+                result.canadians = canadians.canadians;
+                result.version = canadians.version;
                 res.json(result);
             })
         })
